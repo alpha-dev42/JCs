b6d56749c3e0ef18f109d8ede76a750f49916398
Johnny <johnnyly311@gmail.com>
2017-11-30 20:37:38 -0800

    adding products

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171112062115) do
+ActiveRecord::Schema.define(version: 20171130052019) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
     t.string   "image_url"
+    t.string   "price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
