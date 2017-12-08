7c300861f61ea2399631ce9eeae48661fdd27e0e
Johnny <johnnyly311@gmail.com>
2017-12-07 21:47:45 -0800

    added orders view

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -4,4 +4,8 @@ class SimplePagesController < ApplicationController
 
   def contact
   end
+
+  def landing_page
+  	@featured_product = Product.first
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,11 @@ Rails.application.routes.draw do
 
   get 'simple_pages/contact'
 
-  root 'simple_pages#index'
+  root 'simple_pages#landing_page'
   get 'simple_pages/index'
  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+resources :orders, only: [:index, :show, :create, :destroy]
