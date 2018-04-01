102bc0932c183ef9d3575bb99c4730360c3a976a
Johnny <johnnyly311@gmail.com>
2018-04-01 12:04:27 -0700

    adding images

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -6,10 +6,6 @@ class SimplePagesController < ApplicationController
   end
 
   def landing_page
-  	@featured_product = Product.first
-  end
-
-  def landing_page
   @products = Product.limit(3)
 	end
 end
