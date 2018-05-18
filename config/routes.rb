c052ecdc005822a07c425b05d450c1d92cf5e4d7
Johnny <johnnyly311@gmail.com>
2018-05-17 21:44:11 -0700

    5.3 added Contact Form and Thank You message

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
 
   root 'simple_pages#index'
   get 'simple_pages/index'
+
+  # Thank you message after form submission
+  post 'simple_pages/thank_you'
+
  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
