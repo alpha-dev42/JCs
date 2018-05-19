b4950b97d15ed164da5209bf411b9785b7819e28
Johnny <johnnyly311@gmail.com>
2018-05-19 16:59:13 -0700

    5.3 added contact form and html emails

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -10,12 +10,9 @@ class SimplePagesController < ApplicationController
 	end
 
 	def thank_you
-  @name = params[:name]
-  @email = params[:email]
-  @message = params[:message]
-  ActionMailer::Base.mail(from: @email,
-      to: 'johnny.ly209@gmail.com',
-      subject: "A new contact form message from #{@name}",
-      body: @message).deliver_now
+	  @name = params[:name]
+	  @email = params[:email]
+	  @message = params[:message]
+		UserMailer.contact_form(@email, @name, @message).deliver_now
 	end
 end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'johnny.ly209@gmail.com'
   layout 'mailer'
 end
