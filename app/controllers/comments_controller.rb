f4dbaef87c37390d7c4408a94926c73622476b98
Johnny <johnnyly311@gmail.com>
2018-07-23 17:10:14 -0700

    5.9 Validation & Pagination

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,21 +5,21 @@ class CommentsController < ApplicationController
 		@comment.user = current_user
 		
 		respond_to do |format|
-    if @comment.save
-      format.html { redirect_to @product, notice: 'Review was created successfully.' }
-      format.json { render :show, status: :created, location: @product }
-    else
-      format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
-      format.json { render json: @comment.errors, status: :unprocessable_entity }
-    end
-  end
+	    if @comment.save
+	      format.html { redirect_to @product, notice: 'Review was created successfully.' }
+	      format.json { render :show, status: :created, location: @product }
+	    else
+	      format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
+	      format.json { render json: @comment.errors, status: :unprocessable_entity }
+	    end
+  	end
+  end	
 
 	def destroy
 	end
 
 	# Defining comments method to prevent undefined errors
 	private
-
 		def comment_params
 				params.require(:comment).permit(:user_id, :body, :rating)
 		end
