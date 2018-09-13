b99fa60bf8149929c485f823828220d3c931eac6
Johnny <johnnyly311@gmail.com>
2018-09-12 19:31:07 -0700

    added welcome message

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,16 @@
 class UserMailer < ApplicationMailer
-	default from: "from@example.com"
+	default from: "jayscameras@gmail.com"
 
   def contact_form(email, name, message)
-  @message = message
+    @message = message
     mail(from: email,
          to: 'johnny.ly209@gmail.com',
          subject: "A new contact form message from #{name}")
+  end
+
+  def welcome_form(first_name, last_name, email)
+    @appname = "Jay's Cameras"
+   	mail(to: email,
+      	 subject: "Welcome to #{@appname}!")
   end
 end
--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -3,4 +3,9 @@ class UserMailerPreview < ActionMailer::Preview
   def contact_form
   	UserMailer.contact_form("johndoe@example.com", "John Doe	", "Hello World!")
 	end
+
+	def welcome_form
+  	UserMailer.welcome_form("billy", "nob","jay@gmail.com")
+	end
+
 end
