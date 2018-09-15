7efa4e90e6824887a0895cdb0558e5b12a92455d
Johnny <johnnyly311@gmail.com>
2018-09-15 08:42:49 -0700

    updated sign up email

--- app/mailers/user_notifier_mailer.rb
+++ app/mailers/user_notifier_mailer.rb
@@ -1,5 +1,5 @@
 class UserNotifierMailer < ApplicationMailer
-    default :from => 'jayscameras@gmail.com'
+    default :from => 'jay@jayscameras.com'
 
     # send a signup email to the user, pass in the user object that   contains the user's email address
     def send_signup_email(user)
