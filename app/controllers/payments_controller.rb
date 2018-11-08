eaae40e77f0fa36d6024b95ccef8a1527c678a7f
Johnny Ly <johnnyly311@gmail.com>
2018-11-08 00:18:16 -0800

    added Payments controller and configured receipt settings in Stripe

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -2,19 +2,22 @@ class PaymentsController < ApplicationController
     def create
         token = params[:stripeToken]
         # Create the charge on Stripe's servers - this will charge the user's card
-        @product = params[:product_id]
+        @product = Product.find(params[:product_id])
         # Takes in hidden value from _stripe_checkout_button.html.erb
         @user = current_user
         begin
           charge = Stripe::Charge.create(
-            amount: @product.price.to_i*100
-            currency: "usd",
-            source: token,
-            description: params[:stripeEmail]
-          )
+            charge = Stripe::Charge.create(
+              :amount => (@product.price * 100).to_i,
+              :currency => "eur",
+              :source => token,
+              :description => params[:stripeEmail],
+              :receipt_email => params[:stripeEmail]
+            )       
 
           if charge.paid
-            Order.create(product_id, order_id, total)
+            Order.create(product_id: @product.id,user_id: @user.id, total: @product.price)
+            flash[:success] = "Your payment was processed successfully"
           end
 
         rescue Stripe::CardError => e
@@ -23,6 +26,6 @@ class PaymentsController < ApplicationController
           err = body[:error]
           flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
         end
-        redirect_to product_path(product)
-      end
+        redirect_to product_path(@product), notice: "Thank you for your purchase."
+    end
 end
